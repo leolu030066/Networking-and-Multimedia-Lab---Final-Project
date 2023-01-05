2ee665bf24f3b0c5469de5e25e5f7b73daef76cf
tim901231 <b09901020@ntu.edu.tw>
2023-01-05 21:27:16 +0800

    finish_basic_function

--- helpers/create_connection/create_connection.py
+++ helpers/create_connection/create_connection.py
@@ -22,6 +22,11 @@ HOSPITAL_WEBHOOK_PORT = os.getenv('HOSPITAL_WEBHOOK_PORT')
 HOSPITAL_WEBHOOK_HOST = os.getenv('HOSPITAL_WEBHOOK_HOST')
 HOSPITAL_WEBHOOK_BASE = os.getenv('HOSPITAL_WEBHOOK_BASE')
 
+MEDICAL_DEVICE_COMPUTER_ADMIN_URL = os.getenv('MEDICAL_DEVICE_COMPUTER_ADMIN_URL')
+MEDICAL_DEVICE_COMPUTER_WEBHOOK_PORT = os.getenv('MEDICAL_DEVICE_COMPUTER_WEBHOOK_PORT')
+MEDICAL_DEVICE_COMPUTER_WEBHOOK_HOST = os.getenv('MEDICAL_DEVICE_COMPUTER_WEBHOOK_HOST')
+MEDICAL_DEVICE_COMPUTER_WEBHOOK_BASE = os.getenv('MEDICAL_DEVICE_COMPUTER_WEBHOOK_BASE')
+
 THE_WALLET_ADMIN_URL = os.getenv('THE_WALLET_ADMIN_URL')
 THE_WALLET_WEBHOOK_PORT = os.getenv('THE_WALLET_WEBHOOK_PORT')
 THE_WALLET_WEBHOOK_HOST = os.getenv('THE_WALLET_WEBHOOK_HOST')
@@ -195,8 +200,95 @@ async def start_agent():
 
     print("SUCCESS")
     time.sleep(2)
-    await the_wallet_agent_controller.terminate()
+    # await the_wallet_agent_controller.terminate()
     await hospital_agent_controller.terminate()
+
+
+
+
+    time.sleep(10)
+
+    # Inviter
+    # the_wallet_agent_controller = AriesAgentController(admin_url=THE_WALLET_ADMIN_URL)
+
+    # Invitee
+    medical_device_computer_agent_controller = AriesAgentController(admin_url=MEDICAL_DEVICE_COMPUTER_ADMIN_URL)
+
+    # manufacturer_agent_controller.init_webhook_server(
+    #                                         webhook_host=MANUFACTURER_WEBHOOK_HOST,
+    #                                         webhook_port=MANUFACTURER_WEBHOOK_PORT,
+    #                                         webhook_base=MANUFACTURER_WEBHOOK_BASE)
+
+    # await manufacturer_agent_controller.listen_webhooks()
+    #
+    # await seller_agent_controller.listen_webhooks()
+    #
+    #
+    # seller_agent_controller.register_listeners([], defaults=True)
+    # manufacturer_agent_controller.register_listeners([], defaults=True)
+
+    invite = await the_wallet_agent_controller.connections.create_invitation()
+    print("Invite from SELLER", invite)
+
+    the_wallet_connection_id = invite["connection_id"]
+    print("Bob's connection ID for Alice", the_wallet_connection_id)
+
+    response = await medical_device_computer_agent_controller.connections.accept_connection(invite["invitation"])
+
+
+    print("Alice's Connection ID for Bob", response["connection_id"])
+    medical_device_computer_id = response["connection_id"]
+    print("Invite Accepted")
+    print("Alice's state for Bob's connection :", response["state"])
+
+
+    # 
+    time.sleep(10)
+
+    # connection = await seller_agent_controller.connections.get_connection(seller_connection_id)
+    # while connection["state"] != "request":
+    #     time.sleep(1)
+    #     connection = await seller_agent_controller.connections.get_connection(seller_connection_id)
+
+    connection = await the_wallet_agent_controller.connections.get_connection(the_wallet_connection_id)
+    print("Bob's Connection State for Alice :", connection["state"])
+
+    all_conns = await the_wallet_agent_controller.connections.get_connections()
+    print("All Conns : ", all_conns)
+
+
+    connection = await the_wallet_agent_controller.connections.accept_request(the_wallet_connection_id)
+    print("Request Accepted")
+    print(connection)
+
+    print("SELLER AGENT CONNECTION")
+    print(connection)
+
+    while connection["state"] != "active":
+        trust_ping = await the_wallet_agent_controller.messaging.trust_ping(the_wallet_connection_id, "hello")
+        print("TUST PING TO ACTIVATE CONNECTION - SELLER -> RESEARCH")
+        print(trust_ping)
+        time.sleep(5)
+        connection = await the_wallet_agent_controller.connections.get_connection(the_wallet_connection_id)
+
+    trust_ping = await medical_device_computer_agent_controller.messaging.trust_ping(medical_device_computer_id,"hello")
+    print("TUST PING TO ACTIVATE CONNECTION - RESEARCH -> SELLER")
+    print(trust_ping)
+
+    print("MANUFACTURER ID {} SELLER ID {}".format(medical_device_computer_id, the_wallet_connection_id))
+
+    connection = await the_wallet_agent_controller.connections.get_connection(the_wallet_connection_id)
+    print("SELLER AGENT CONNECTION")
+    print(connection)
+
+    connection = await medical_device_computer_agent_controller.connections.get_connection(medical_device_computer_id)
+    print("RESEARCH AGENT CONNECTION")
+    print(connection)
+
+    print("SUCCESS")
+    time.sleep(2)
+    # await the_wallet_agent_controller.terminate()
+    await medical_device_computer_agent_controller.terminate()
 
 if __name__ == "__main__":
     # time.sleep(60)
